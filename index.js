b7dcefb406b0f84e8cd21e8f25d9d15df1c0ccff
Vladimir Nazarov <ruvater@gmail.com>
2017-08-09 12:36:11 +0300

    Added wc -l script

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const fileName = process.argv[2] || ''
+
+if (!fileName.length) {
+  process.stderr.write('Filename is not specified\n')
+  process.exit(1)
+}
+
+let lineCount = 0
+
+fs.createReadStream(process.argv[2])
+  .on('data', function(dataChunk) {
+    for (let char of dataChunk) {
+      if (char === '\n'.charCodeAt()) {
+        lineCount++
+      }
+    }
+  })
+  .on('end', function() {
+    process.stdout.write(`${lineCount}\n`)
+  });
